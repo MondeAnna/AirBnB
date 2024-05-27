2141cca6911e9bb066141bb6e15d9f23ce9bd8eb
MondeAnna <monde.anna@gmail.com>
2024-05-27 03:14:44 +0200

    storage (reload): execute `reload` at proj init

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,9 @@
+#!/usr/bin/python3
+"""Pre-Initial vital system components"""
+
+
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
+
+
+storage = FileStorage()
+storage.reload()
